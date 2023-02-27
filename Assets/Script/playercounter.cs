774c2813c93a461843ad251332bf04f769e1d8dc
Osmanthus1 <asaiahrenodo@gmail.com>
2023-02-26 19:31:43 -0500

    Draft 2.7

--- Assets/Script/playercounter.cs
+++ Assets/Script/playercounter.cs
@@ -7,12 +7,30 @@ public class playercounter : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 currentPos = transform.position;
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            currentPos.x = currentPos.x + 3 * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            currentPos.x = currentPos.x - 3 * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            currentPos.z = currentPos.z - 3 * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            currentPos.z = currentPos.z + 3 * Time.deltaTime;
+        }
+        transform.position = currentPos;
     }
 }
