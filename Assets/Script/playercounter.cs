43a324868b485357bc7207007c4cde2c27b34b6d
Osmanthus1 <asaiahrenodo@gmail.com>
2023-02-27 23:01:02 -0500

    Draft 3.0

--- Assets/Script/playercounter.cs
+++ Assets/Script/playercounter.cs
@@ -1,13 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class playercounter : MonoBehaviour
 {
+    int health = 3;
+
+    public TMP_Text healthText;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        healthText.text = health.ToString();
     }
 
     // Update is called once per frame
